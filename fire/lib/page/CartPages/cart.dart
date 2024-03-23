c20485726f7caabe1518e90f390317de71c3149c
Enkutatash <enkutatasheshetu96.com>
2024-03-23 13:05:13 +0300

    working on cart page

--- fire/lib/page/CartPages/cart.dart
+++ fire/lib/page/CartPages/cart.dart
@@ -25,28 +25,38 @@ class Cart extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body:Obx(()=>_cartController.product.isEmpty?const Center(
-                  child: Text("Empty cart"),
-                ):SizedBox(
-                  height:height*0.7
-                  ,child: ListView.builder(
-                    itemCount: _cartController.product.length,
-                    itemBuilder: (context,index){
-                      return CartItem(
-                            _cartController,
-                            _cartController.product.keys.toList()[index],
-                            _cartController.product.values.toList()[index],
-                            index,
-                      ); 
-                    }),
-                )
+      body:Obx(() => _cartController.product.isEmpty
+    ? const Center(
+        child: Text("Empty cart"),
+      )
+    : SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: height * 0.7, // Adjust height as needed
+              child: ListView.builder(
+                itemCount: _cartController.product.length,
+                itemBuilder: (context, index) {
+                  return CartItem(
+                    _cartController,
+                    _cartController.product.keys.toList()[index],
+                    _cartController.product.values.toList()[index],
+                    index,
+                  );
+                },
+              ),
             ),
+            
+          ],
+        ),
+      )),
+
           );
         }
       }
 
 class CartItem extends StatelessWidget {
-   final CartController _cartController;
+  final CartController _cartController;
   final Item2 product;
   final int quantity;
   final int index;
@@ -57,37 +67,66 @@ class CartItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return Container(
+      color: Color(0XFFF8F7F7),
+      padding: EdgeInsets.all(10),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-              child: Image.network(
-                  height: height * 0.13,
-                  width: width * 0.4,
-                  fit: BoxFit.cover,
-                  product.imageURL),
-            ),
-            IconButton(
-                        onPressed: () {
-                          _cartController.removeProduct(product);
-                        },
-                        icon:const Icon(
-                          Icons.remove_circle,
-                          color: Color(0XFF6055D8),
-                          size: 30,
-                        )),
-                    Text('$quantity'),
-                    IconButton(
-                        onPressed: () {
-                          _cartController.addProduct(product);
-                        },
-                        icon:const Icon(
-                          Icons.add_circle,
-                          color: Color(0XFF6055D8),
-                          size: 30,
-                        )),
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(
+                height: height * 0.13,
+                width: width * 0.4,
+                fit: BoxFit.cover,
+                product.imageURL),
+          ),
+          Column(
+            children: [
+              Text(
+                product.itemName,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text('${product.cost}')
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    _cartController.delete(product);
+                  },
+                  icon: Icon(
+                    Icons.delete_sharp,
+                    color: Color(0XFFF65A5A),
+                    size: 30,
+                  )),
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        _cartController.removeProduct(product);
+                      },
+                      icon: Icon(
+                        Icons.remove_circle,
+                        color: Color(0XFF6055D8),
+                        size: 30,
+                      )),
+                  Text('${quantity}'),
+                  IconButton(
+                      onPressed: () {
+                        _cartController.addProduct(product);
+                      },
+                      icon: Icon(
+                        Icons.add_circle,
+                        color: Color(0XFF6055D8),
+                        size: 30,
+                      )),
+                ],
+              )
+            ],
+          ),
         ],
       ),
     );
@@ -100,7 +139,9 @@ class CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Container(
+      height: 50,
       padding:const EdgeInsets.all(8),
       child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
